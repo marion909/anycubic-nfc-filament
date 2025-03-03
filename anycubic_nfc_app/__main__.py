b1f6de4bb30ef996690d1d308908d47e3270467e
Molodos <michael-weichenrieder@t-online.de>
2025-03-03 22:48:07 +0100

    Added density to ui

--- anycubic_nfc_app/__main__.py
+++ anycubic_nfc_app/__main__.py
@@ -81,5 +81,5 @@ if __name__ == "__main__":
     App init point
     """
     # write_spool()
-    read_spool_raw()
-    # start_web_app(8080, debug=True)
+    # read_spool_raw()
+    start_web_app(8080, debug=True)
